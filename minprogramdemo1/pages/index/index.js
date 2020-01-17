e5fd255586cdb9ac41e58b5b0e1d8354674b48a0
songdesheng <song201703@163.com>
2020-01-17 11:33:05 +0800

    首页静态页面完成

--- minprogramdemo1/pages/index/index.js
+++ minprogramdemo1/pages/index/index.js
@@ -1,41 +1,102 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
 Page({
-  data: {
-    focus: false,
-    inputValue: ''
+  onShareAppMessage() {
+    
   },
-  bindKeyInput: function (e) {
-    this.setData({
-      inputValue: e.detail.value
+  
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    swiper_list:[],
+    indicatorDots: true,
+    navigations:[],
+    floorlist:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/floordata',
+      success:(res)=>{
+        console.log(res)
+        this.setData({
+          floorlist:res.data.message
+        }) 
+      }
+    })
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/catitems',
+      success:(res)=>{
+        console.log(res)
+        this.setData({
+          navigations:res.data.message
+        }) 
+      }
+    })
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+      success:(res)=>{
+        console.log(res)
+        this.setData({
+          swiper_list:res.data.message
+        }) 
+      }
     })
   },
-  bindReplaceInput: function (e) {
-    var value = e.detail.value
-    var pos = e.detail.cursor
-    var left
-    if (pos !== -1) {
-      // 光标在中间
-      left = e.detail.value.slice(0, pos)
-      // 计算光标的位置
-      pos = left.replace(/11/g, '2').length
-    }
-
-    // 直接返回对象，可以对输入进行过滤处理，同时可以控制光标的位置
-    return {
-      value: value.replace(/11/g, '2'),
-      cursor: pos
-    }
-
-    // 或者直接返回字符串,光标在最后边
-    // return value.replace(/11/g,'2'),
+  changeIndicatorDots() {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
   },
-  bindHideKeyboard: function (e) {
-    if (e.detail.value === '123') {
-      // 收起键盘
-      wx.hideKeyboard()
-    }
+  
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
   }
 })
